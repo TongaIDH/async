65753ec81d1327f45ec3ca21ba5f21830f901ee4
Gaston Garcia <gastongarcia@MacBook-Pro-de-restore.local>
2024-12-07 11:58:33 -0300

    gastongarcia/async-await-challenge-complete

---
+++ asyncAwait/challenge.js
@@ -0,0 +1,25 @@
+import fetch from 'node-fetch';
+
+const API = 'https://api.escuelajs.co/api/v1';
+
+async function fetchData(apiUrl) {
+    const response = await fetch(apiUrl);
+    const data = await response.json()
+    return data;
+}
+
+const anotherFn = async (apiUrl) => {
+    try {
+        const products = await fetchData(`${apiUrl}/products`);
+        const product = await fetchData(`${apiUrl}/products/${products[0].id}`);
+        const category = await fetchData(`${apiUrl}/categories/${product.category.id}`);
+
+        console.log(products);
+        console.log(product.title);
+        console.log(category.name);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+anotherFn(API);
